86620864964f5c9ec58543ec0cd4d7246add61f2
Rafi Mahafid <rafimahafid@gmail.com>
2022-04-11 00:08:31 +0600

    init commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { StringDecoder } = require('string_decoder');
 const url = require('url');
 
 const app = {};
@@ -19,8 +20,21 @@ app.requestListener = (req, res) => {
     // console.log(parseUrl);
     res.end('Hello programmers!');
     const method = req.method.toLowerCase();
+    console.log(method);
     const headerObject = req.headers;
     console.log(headerObject);
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+    });
 };
 
 app.createServer();
